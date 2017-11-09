9fcba877971c369c4ef5d0b68e86a2fbf9960a9d
sudowind <wfpkueecs@163.com>
2017-11-09 16:42:43 +0800

    add user class

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Time Variant Community Model
+A simulation model for users around AP router
+
 [1] W.-j. Hsu, T. Spyropoulos, K. Psounis, A. Helmy, Modeling time-variant user mobility in wireless mobile networks, in: INFOCOM 2007. 26th IEEE
 International Conference on Computer Communications. IEEE, pp. 758–766
--- src/tvcm.py
+++ src/tvcm.py
@@ -1,4 +1,27 @@
+class User(object):
+
+    def __init__(self, user_edge_length):
+        self.user_edge_length = user_edge_length
+        self.roaming_epoch_length = 0
+        self.local_epoch_length = 0
+        self.p_l = 0    # roaming to local
+        self.p_r = 0    # local to roaming
+        self.pause_time_range = []
+        self.speed_range = []
+
+
 class TimeVariantCommunityModel(object):
 
-    def __init__(self):
+    def __init__(self, area_edge_length):
+        """
+        Init simulation area
+        :param area_edge_length:
+        """
+        self.area_edge_length = area_edge_length
+        self.users = []
+
+    def gen_users(self, user_count):
+        self.users = [User(10) for _ in range(user_count)]
+
+    def start_simulation(self):
         pass
